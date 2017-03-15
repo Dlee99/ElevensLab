7c4edfdac7bbced9661d38d2d16b5037da2a706c
Dlee99 <Dereklee9ab@aol.com>
2017-03-15 16:52:38 -0400

    Initial commit

---
+++ src/com/company/CardTester.java
@@ -0,0 +1,24 @@
+package com.company;
+
+/**
+ * This is a class that tests the Card class.
+ */
+public class CardTester {
+
+	/**
+	 * The main method in this class checks the Card operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		Card c1, c2, c3;
+		c1 = new Card("Jack", "Hearts", 11);
+		c2 = new Card("Jack", "Hearts", 11);
+		c3 = new Card("Ace", "Spade", 1);
+		System.out.println(c1.toString());
+		System.out.println(c1.rank());
+		System.out.println(c1.suit());
+		System.out.println(c1.pointValue());
+		System.out.println(c1.matches(c2));
+		System.out.println(c1.matches(c3));
+	}
+}
