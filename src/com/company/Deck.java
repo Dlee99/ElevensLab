13324b9fe497bc61624fd00eb6f4e9a58054f3a3
dl447 <dereklee9ab@aol.com>
2017-03-21 10:33:25 -0400

    finish perfectshuffle and selection shuffle

--- src/com/company/Deck.java
+++ src/com/company/Deck.java
@@ -34,7 +34,7 @@ public class Deck {
 	public Deck(String[] ranks, String[] suits, int[] values) {
 		cards = new ArrayList<Card>();
 		for(int i = 0; i < ranks.length; i++){
-			for(int j = 0; j < suits.length; i++) {
+			for(int j = 0; j < suits.length; j++) {
 				cards.add(new Card(ranks[i], suits[j], values[i]));
 			}
 		}
